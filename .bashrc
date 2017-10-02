65283bdc9f33e4bd8fb3750e66ae89afb8e406c9
dooooooooinggggg <dooooooooinggggg@gmail.com>
2017-10-02 16:58:29 +0900

    update

--- .bashrc
+++ .bashrc
@@ -2,3 +2,4 @@ PS1="\[\033[31m\][\u@\h:\w] \t \n\$\[\033[0m\] "
 #export HOMEBREW_CASK_OPTS=“—appdir=/Applications”
 
 alias vagssh='vagrant up;vagrant ssh'
+export PATH=$PATH:/usr/local/Cellar/llvm/HEAD-1d6becb/bin
--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
-# vimフォルダのやつも作る
 ln -sf ~/dotfiles/.vim ~/.vim
+
+wget http://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zsh* ~/
 
 source ~/.bash_profile
+source ~/.zshrc
