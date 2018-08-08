690ea36ae23ffaa5c8c32ec44767aef02da91916
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-08-08 14:26:18 +0900

    Sloppy Commit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # private repository
 # git clone https://github.com/dooooooooinggggg/ShellUtil.git ~/ShellUtil
 # sh ~/ShellUtil/pkg_manager_init/init_brew.sh
+# mkdir ~/OSS
 
 # pyenv
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
@@ -13,14 +14,28 @@ ln -sf ~/dotfiles/.vim ~/.vim
 wget http://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
 # shell config
+
+#bash
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+# zsh
 ln -sf ~/dotfiles/.zsh* ~/
+
 
 # vscode
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
 
-mkdir ~/OSS
 
+#########################
+# /etc/shellsに以下を追記 #
+#########################
+vi /etc/shells
+# /usr/local/bin/zsh
+
+# change default shell
+chsh -s /usr/local/bin/zsh
+
+
+# 対応しているシェルに合わせてsourceを実行し、設定を読み込む
 # source ~/.bash_profile
 source ~/.zshrc
