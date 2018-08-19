dbc2b82a0597d0c75c0fbcc2fc0ec498702ebed6
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-08-19 15:49:44 +0900

    Sloppy Commit

--- setup.sh
+++ setup.sh
@@ -20,18 +20,19 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 
 # vscode
-extensions=$(cat vscode/plugins)
-for extension in $extensions; do
-    echo "install $extension ..."
-    code --install-extension $extension
-done
-mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/orig_settings.json
-ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
-
-echo "/usr/local/bin/zsh" >> /etc/shells
+if [ "$(uname)" == 'Darwin' ]; then
+  extensions=$(cat vscode/plugins)
+  for extension in $extensions; do
+      echo "install $extension ..."
+      code --install-extension $extension
+  done
+  mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/orig_settings.json
+  ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
+fi
 
 # change default shell
 if [ "$(uname)" == 'Darwin' ]; then
+  echo "/usr/local/bin/zsh" >> /etc/shells
   chsh -s /usr/local/bin/zsh
 fi
 # enter password
